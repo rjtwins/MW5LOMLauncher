ad1dd97166209c26b6d17692af8b19db134116a4
Jorre Vedder <jorre_vedder@hotmail.com>
2020-06-23 22:48:32 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MW5LOMLauncherV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Logic logic = new Logic(this);
+            Application.DoEvents();
+            this.timer1.Enabled = true;
+            this.timer1.Start();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Logic.StartMainProgram();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿using Newtonsoft.Json;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Application = System.Windows.Forms.Application;
+
+namespace MW5LOMLauncherV2
+{
+    static class Program
+    {
+        public static Form1 Form1;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Program.Form1 = new Form1();
+            Application.Run(Program.Form1);
+        }
+    }
+
+    public class Logic
+    {
+        public static Logic logic;
+        ProgramData ProgramData = new ProgramData();
+        string ProgramDataPath = "";
+        string LatestVersion = "";
+        bool ExeExists = false;
+
+        public Logic(Form1 form1)
+        {
+            Logic.logic = this;
+            this.ProgramDataPath = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) +
+                @"\MW5LoadOrderManager";
+
+            bool update = false;
+
+            var client = new GitHubClient(new ProductHeaderValue("MW5LoadOrderManagerUpdater"));
+            this.LatestVersion = client.Repository.Release.GetLatest("rjtwins", "MW5-Mod-Manager").Result.TagName;
+
+            //Is this our first time running?
+            if (LoadGenProgramData())
+            {
+                form1.listBox1.Items.Add("First time startup or corrupted files detected, staring update/restore...");
+                Console.WriteLine("First time startup or corrupted files detected, staring update/restore...");
+
+                //we are running a "fresh" version with no stored program data or a corrupt program data file.
+                update = true;
+            }
+            else
+            {
+                form1.listBox1.Items.Add("Checking for updates...");
+                form1.listBox1.Items.Add(String.Format("The latest release is tagged at {0} we are running {1}",
+                    LatestVersion,
+                    this.ProgramData.version));
+
+                Console.WriteLine("Checking for updates...");
+                Console.WriteLine(
+                    "The latest release is tagged at {0} we are running {1}",
+                    LatestVersion,
+                    this.ProgramData.version);
+
+                if (this.ProgramData.version < float.Parse(LatestVersion))
+                {
+                    form1.listBox1.Items.Add("A new version is available, starting update...");
+                    Console.WriteLine("A new version is available, starting update...");
+                    update = true;
+                }
+            }
+
+            if (update)
+            {
+                try
+                {
+                    using (var webClient = new WebClient())
+                    {
+                        string url = String.Format("https://github.com/rjtwins/MW5-Mod-Manager/releases/download/{0}/MW5.Mod.Manager.exe", this.LatestVersion);
+                        webClient.DownloadFile(url, this.ProgramDataPath + @"\MW5 Mod Manager.exe");
+                    }
+                    this.ProgramData.version = float.Parse(this.LatestVersion);
+
+                    //Update program data
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Formatting = Formatting.Indented;
+                    using (StreamWriter sw = new StreamWriter(ProgramDataPath + @"\ProgramData.json"))
+                    using (JsonWriter writer = new JsonTextWriter(sw))
+                    {
+                        serializer.Serialize(writer, this.ProgramData);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+            form1.listBox1.Items.Add("Done, starting MW5 Load Order Manager");
+            Console.WriteLine("Done, starting MW5 Mod Loader Manager");
+        }
+
+        internal static void StartMainProgram()
+        {
+            //Start the main program
+            System.Diagnostics.Process.Start(Logic.logic.ProgramDataPath + @"\MW5 Mod Manager.exe");
+        }
+
+        public bool LoadGenProgramData()
+        {
+            //Load install dir from previous session:
+            if (!File.Exists(ProgramDataPath + @"\ProgramData.json"))
+            {
+                System.IO.Directory.CreateDirectory(ProgramDataPath);
+                System.IO.File.Create(ProgramDataPath + @"\ProgramData.json").Close();
+                this.ProgramData.vendor = "EPIC";
+                this.ProgramData.installdir = "";
+                this.ProgramData.version = 0f;
+                Console.WriteLine(ProgramDataPath + @"\ProgramData.json was not found.");
+                return true;
+            }
+            try
+            {
+                string json = File.ReadAllText(ProgramDataPath + @"\ProgramData.json");
+                this.ProgramData = JsonConvert.DeserializeObject<ProgramData>(json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(ProgramDataPath + @"\ProgramData.json unreadable.");
+                //Console.WriteLine(e.Message);
+                //Console.WriteLine(e.StackTrace);
+                return true;
+            }
+            if (this.ProgramData.version <= 0f)
+            {
+                Console.WriteLine("Version outside of possible range.");
+                return true;
+            }
+
+
+            if (File.Exists(ProgramDataPath + @"\MW5 Mod Manager.exe"))
+            {
+                ExeExists = true;
+            }
+            //ProgramData.json is fine but the exe file is missing? Update to get it.
+            else
+            {
+                Console.WriteLine(ProgramDataPath + @"\MW5 Mod Manager.exe not found.");
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class ProgramData
+    {
+        public string vendor { set; get; }
+        public float version { set; get; }
+        public string installdir { set; get; }
+    }
+}
